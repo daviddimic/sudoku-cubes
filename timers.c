2809330bd34243b9634289a0e4b6100c8434dac6
David Dimic <daviddimic@hotmail.com>
2017-12-29 14:35:36 +0100

    dodat tajmer kretanja

--- timers.c
+++ timers.c
@@ -4,12 +4,27 @@
 #include <GL/glut.h>
 #include <assert.h>
 
+/* externe promenljive */
 int timer_rotate_active = 0;
+int timer_move_active = 0;
+/* za tajmer okretanja kocke on_timer_wsad */
 char unsigned wsad_key;
+/* tekuca tabla */
 int curr_table;
+/* rotacija kocke */
 float x_rotation, y_rotation;
+/* pozicija kamera */
+float camera_x, camera_y, camera_z;
+/* parametri kretanaj kocke */
+float x_t, y_t;
 
-/* za koliko treba rotirati kocku i kada zaustaviti tajmer */
+/* vreme animacije - za kretanje kocke */
+static float t;
+/*TODO rand menjati pocetna brzina kretanja kocke */
+static const float v0 = 6;
+
+
+/* pomocna f-ja: za koliko treba rotirati kocku i kada zaustaviti tajmer */
 static void set_rotation_and_timer(float *rotation, int *timer){
     float eps = 0.01;
 
@@ -92,5 +107,28 @@ void on_timer_wsad(int value) {
 
     if(timer_rotate_active){
         glutTimerFunc(TIMER_ROTATE_WAIT, on_timer_wsad, TIMER_ROTATE_ID);
+    }
+}
+
+
+/* TODO izmeniti tajmer za kretanje kocke */
+void on_timer_move(int value) {
+
+    if(value != TIMER_MOVE_ID) return;
+
+    t += 0.05;
+
+    x_t = v0*t*t;
+    y_t = -t*t/(v0-2);
+
+    // da upadne u kocku
+    if(IN(x_t - 7 - camera_x, -EPS, EPS) && IN(y_t + 1 - camera_y, - EPS, EPS)){
+        timer_move_active = 0;
+    }
+
+    glutPostRedisplay();
+
+    if(timer_move_active){
+        glutTimerFunc(TIMER_MOVE_WAIT, on_timer_move, TIMER_MOVE_ID);
     }
 }
--- timers.h
+++ timers.h
@@ -1,13 +1,30 @@
 #ifndef TIMER_H_
-#define  TIMER_H_
+#define TIMER_H_
 
+/* parametri tajmera rotacije */
 #define TIMER_ROTATE_WAIT 50
 #define TIMER_ROTATE_ID 0
 
+/* parametri tajmera kretanja */
+#define TIMER_MOVE_WAIT 20
+#define TIMER_MOVE_ID 1
+
+/* makro koji prverava da li je X izmedju A i B*/
+#define IN(X, A, B) (((X) >= (A)) && ((X) <= (B)))
+#define EPS 0.2
+
+/* gravitaciona konstanta i broj PI - za kretanje kocke */
+#define G 9.81
+#define pi 3.141592654
+
 extern int timer_rotate_active;
+extern int timer_move_active;
 extern unsigned char wsad_key;
+
 
 /* tajmer za okretanje kocke */
 void on_timer_wsad(int value);
+/* tajmer za kretanje kocke */
+void on_timer_move(int value);
 
 #endif /*TIMER_H_*/
