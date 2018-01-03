914389c5e31bd1145e973ad31a2c29578fd10c70
David Dimic <daviddimic@hotmail.com>
2018-01-03 15:40:42 +0100

    popravljeno rotiranje kocke

--- timers.c
+++ timers.c
@@ -69,13 +69,21 @@ void on_timer_wsad(int value) {
             wsad_key == 'a' ||
             wsad_key == 'd');
 
+    /* NOTE
+     * curr_table ovde treba shvatiti kao SLEDECA tabla
+     * jer se prvo postavlja curr_table sledece table pa tek onda vrsi rotacija
+     */
+
     switch (wsad_key) {
         case 'w':
             /* ne moze se ici napred ako smo na gornjoj tabli */
             if(curr_table == BACK)
                 curr_table = UP;
-            else
-                x_rotation += 10;
+            else /* ne moze se ici gore ako smo ga donjoj tabli koja je rotirana */
+                if(curr_table != UP && curr_table != DOWN && y_rotation)
+                    curr_table = DOWN;
+                else
+                    x_rotation += 10;
 
             set_rotation_and_timer(&x_rotation, &timer_rotate_active);
         break;
@@ -84,8 +92,11 @@ void on_timer_wsad(int value) {
             /* ne moze se ici dole ako smo na donjoj tabli */
             if(curr_table == BACK)
                 curr_table = DOWN;
-            else
-                x_rotation -= 10;
+            else /* ne moze se ici dole ako smo ga gornjoj tabli koja je rotirana */
+                if(curr_table != UP && curr_table != DOWN && y_rotation)
+                    curr_table = UP;
+                else /* rotacija se vrsi tek ako ova dva uslova ne prodju */
+                    x_rotation -= 10;
 
             set_rotation_and_timer(&x_rotation, &timer_rotate_active);
         break;
@@ -160,14 +171,7 @@ static void lineAB(float Ax, float Az, float Bx, float Bz, float t){
     z_t = Az + t*(Bz - Az);
 }
 
-/* da li je kocka pogodila kameru */
-static int in_camera(){
-    /* skok kamere visi od kocke */
-    if(jump + camera_y < 1 /*TODO velicina kocke*/)
-        return 1;
 
-    return 0;
-}
 
 void on_timer_move(int value) {
 
@@ -178,14 +182,13 @@ void on_timer_move(int value) {
     camera_y = 0.2;
     camera_z = 1;
 
-
     move_t += 0.03;
+
     /* duz od rand tacke do kamere */
     lineAB(cube_start_x, cube_start_z, camera_x, camera_z, move_t);
 
-
     if(move_t >= 0.99 && move_t <= 1.05) {
-        if(in_camera()){
+        if(cube_in_camera(jump, camera_y, 1)){
             /* ako je pogodjena kamera kocka se stavlja u (0,0) i resava se kocka */
             /* inicijalizuje se nova kocka */
             init_tables(tables, NUM_TABLES, N, &help_number, &start_time);
